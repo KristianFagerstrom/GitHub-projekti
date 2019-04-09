a60ccbf5d9d5f42d561cc544585e0f89b4f29bca
GalaTheFish <arttu.v@live.com>
2019-04-09 11:21:22 +0300

    suomea ja muuta

--- gitti_projekti/KOOP2018.php
+++ gitti_projekti/KOOP2018.php
@@ -3,13 +3,37 @@
 <header>
 <style>
 	body {
-		text-align: left; // center;
-		background-color: white;
+    text-align: center;
+// center;
+    background-color: white;
+    background-size: 100% auto;
+    color: #FFFFFF;
+    font-family: "Gill Sans", "Gill Sans MT", "Myriad Pro", "DejaVu Sans Condensed", Helvetica, Arial, sans-serif;
 	}	
 	textarea {
-		background-color: white;
+    background-color: white;
+    text-align: center;
 	}
 	
+body div {
+    margin-left: auto;
+    margin-right: auto;
+    width: 70%;
+}
+	.loader {
+    border: 16px solid #f3f3f3;
+    border-top: 16px solid #3498db;
+    border-radius: 50%;
+    width: 120px;
+    height: 120px;
+    animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
 </style>
 </header>
 
@@ -19,22 +43,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 	$file = 'kooprapo_1.txt'; 
 	if (is_writable($file)) 
 		{
-		// Ensin tyhjätään tiedot...
-		file_put_contents( $file, "xls" . PHP_EOL); 
+		// Ensin tyhj&#228;t&#228;&#228;n tiedot...
+		file_put_contents( $file, PHP_EOL); 
 		
-		// Laitetaan uudet TIEDOT xls:n jälkeen alapuolelle
-		$KirjausPVM = date("Y/m/d"). " kello:" . date("h:i:s") ;
-		file_put_contents( $file, $KirjausPVM .  " KOULUTUS/OPPISOPIMUSAIHIO" . PHP_EOL, FILE_APPEND); 
-		file_put_contents( $file, "KERÄTYT TIEDOT:" .    " " . PHP_EOL, FILE_APPEND);
+		// Laitetaan uudet TIEDOT xls:n j&#228;lkeen alapuolelle
+		file_put_contents( $file, $KirjausPVM .  "Asiakkaan tiedot" . PHP_EOL, FILE_APPEND); 
 		file_put_contents( $file, $_POST['r1'] . " " . PHP_EOL, FILE_APPEND); 
 		file_put_contents( $file, $_POST['r2'] . " " . PHP_EOL, FILE_APPEND); 
-		file_put_contents( $file, "pp.kk.vv"   . " " . PHP_EOL, FILE_APPEND); //Exceliä varten 
-		file_put_contents( $file, "pp.kk.vv"   . " " . PHP_EOL, FILE_APPEND); //Exceliä varten		
 		file_put_contents( $file, $_POST['r22'] . " " . PHP_EOL, FILE_APPEND); 
 		file_put_contents( $file, $_POST['r23'] . " " . PHP_EOL, FILE_APPEND);  
 		file_put_contents( $file, $_POST['r25'] . " " . PHP_EOL, FILE_APPEND); 
 		file_put_contents( $file, $_POST['r27'] . " " . PHP_EOL, FILE_APPEND); 
-		file_put_contents( $file, "*LOPPU* Voit viedä tiedot esim. Exceliin tästä" . " " . PHP_EOL, FILE_APPEND);
+		file_put_contents( $file, PHP_EOL, FILE_APPEND);
 		}
 	else { print 'Suojausongelmia?';} 	
 		print '****************************** <br>' ;
@@ -42,15 +62,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 		print '****************************** <br><br>' ;
 		print '<form action="" id="form1">';
 		print '<h1> Voit tarkastaa tiedot ... </h1>' ;
-		print '<br> Paina tästä tarkastaaksesi tiedot  <button formtarget="_blank" type="submit" formaction="kooprapo_1.txt" > Tiedot näkyviin! </button>';
-		print '<br> (Voit palata takasin ja lähettää tiedot sen jälkeen eteenpäin) ' ;
-		print '<h1> Voit siirtyä takaisin ja tallentaa uudet tiedot...</h1>' ;
-		print '<br> Paina tästä siirtyäksesi takaisin  <button formtarget="_parent" type="submit"  > Takaisin! </button>';
+		print '<br> Paina t&#228;st&#228; tarkastaaksesi tiedot  <button formtarget="_blank" type="submit" formaction="kooprapo_1.txt" > Tiedot n&#228;kyviin! </button>';
+		print '<br> (Voit palata takasin ja l&#228;hett&#228;&#228; tiedot sen j&#228;lkeen eteenp&#228;in) ' ;
+		print '<h1> Voit siirty&#228; takaisin ja tallentaa uudet tiedot...</h1>' ;
+		print '<br> Paina t&#228;st&#228; siirty&#228;ksesi takaisin  <button formtarget="_parent" type="submit"  > Takaisin! </button>';
 		print '</form>';
-		print '<h1> Lopuksi voit lähettää tiedot haluamaasi s-postiin...</h1>' ;		
+		print '<h1> Lopuksi voit l&#228;hett&#228;&#228; tiedot haluamaasi s-postiin...</h1>' ;		
 		print '<form action="SEND2018.php" method="post" id="sendme" >';
-		print '<br> <br> <input name="spostia" type="email" maxlength="55" placeholder="esimerkiksi burot@bc.fi" size="25" > ';
-		print '<input type="submit" name="submit" value="Lähetä s-posti tästä!" form="sendme" >';
+		print '<br> <br> <input name="spostia" type="email" maxlength="55" placeholder="S&#228;k&#246;posti osoitteesi" size="25" > ';
+		print '<input type="submit" name="submit" value="L&#228;het&#228; s-posti t&#228;st&#228;!" form="sendme" >';
 		print '</form>';
 		
 		print '<br> *-*';
@@ -58,28 +78,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 else 
 {
 ?>
-<h1> VAAK <br> Opinto Tehtävä </h1>
+	<div>
+<h1> K&#196;&#196;K <br> Anna meille tietosi </h1>
 <h3> Gitti Projekti  </h3>
 <br>
 <form action="KOOP2018.php" method="post">
-<h3> Opiskelijan tiedot </h3>
-Opiskelijan nimi<br><textarea   maxlength="50" placeholder="Sukunimi Etunimi" name="r1" rows="2" cols="85" ></textarea> <br>
-Ryhmän tunnus<br><textarea maxlength="50" placeholder="Opiskelijaryhmän tunnus" name="r2" rows="2" cols="85" ></textarea> <br>
+<h3> Sinun tiedot </h3>
+Nimesi<br><textarea   maxlength="50" placeholder="Sukunimi Etunimi" name="r1" rows="2" cols="85" ></textarea> <br>
 <!-- 1PVM <br><textarea maxlength="6" placeholder="PVM" name="r3" rows="1" cols="85" ></textarea> <br> -->
 <!-- 2PVM <br><textarea maxlength="6" placeholder="PVM" name="r4" rows="1" cols="85" ></textarea> <br> -->
 	  
-Sähköposti    
-<br><input   type="email" maxlength="150" placeholder="sähköposti" name="r22" size="84" rows="2" cols="84" ><br>
-Käyntiosoite  
-<br><textarea maxlength="50" placeholder="käyntiosoite jos eri kuin yllä" name="r23" rows="2" cols="85" ></textarea> <br>
-Puhelin 
-<br><textarea maxlength="50" placeholder="puhelin" name="r25" rows="2" cols="85" ></textarea> <br>
+S&#228;hk&#246;posti    
+<br><textarea   type="email" maxlength="150" placeholder="s&#228;hk&#246;posti" name="r22" size="84" rows="2" cols="84" ></textarea><br>
+Osoite  
+<br><textarea maxlength="50" placeholder="Anna osoitteesi, lupaamme ett&#228; emme murtaudu taloosi." name="r23" rows="2" cols="85" ></textarea> <br>
+Puhelin-numero
+<br><textarea maxlength="50" placeholder="puhelin-numero" name="r25" rows="2" cols="85" ></textarea> <br>
 MUUTA    
-<br><textarea maxlength="50" placeholder="...lisätietoja..." name="r27" rows="2" cols="85" ></textarea> <br>
+<br><textarea maxlength="50" placeholder="...lis&#228;tietoja..." name="r27" rows="2" cols="85" ></textarea> <br>
 <br>
-JATKA PAINAMALLA TÄSTÄ! <input type="submit" name="submit" value="PAINA" />
+Jatka Painamalla T&#228;st&#228;! <input type="submit" name="submit" value="PAINA" />
 <?php 
 }  
 ?>
-
+</div>
 </html>
--- gitti_projekti/SEND2018.php
+++ gitti_projekti/SEND2018.php
@@ -17,15 +17,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 // PAKOLLISTA content-type when sending HTML email
 	$headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-	$headers .= 'From: <koop.sovellus>' . "\r\n";
+	$headers .= 'From: <kysely.botti>' . "\r\n";
 // $headers .= 'Cc: myboss@example.com' . "\r\n";
-// Lähetetään vastaanottaja, aihe, viestin sisältö,
+// Lähetetään vastaanottaja, aihe, viestin sisältö, 
 // https://www.w3schools.com/php/func_mail_mail.asp
-	mail($_POST['spostia'],"KOOPSOPIMUS",$teksti, "$headers" );	
-echo "Viesti on nyt lähetetty: " . $_POST['spostia'] . " --> <br><br>  ";
-echo "Tiedot ovat myös poistettu palvelimelta <br> Kiitos! " . " --> <br><br> VOIT NYT SULKEA IKKUNAN ";
+	mail($_POST['spostia'],"moi :)",$teksti, "$headers" );	
+echo "Viesti on nyt l&#228;hetetty: " . $_POST['spostia'] . " --> <br><br>  ";
+echo "Tiedot ovat my&#246;s poistettu palvelimelta <br> Kiitos! " . " --> <br><br> VOIT NYT SULKEA IKKUNAN ";
 }
 ?>
 </body>
 </html>
-
